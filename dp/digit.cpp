56d25ebdd03431097e01c4cb3f8e955e5fec3f2b
Oyster <eason9506@gmail.com>
2024-12-18 22:06:48 +0800

    fix digit.cpp

--- dp/digit.cpp
+++ dp/digit.cpp
@@ -1,16 +1,10 @@
-ll dp[MXN_BIT][PRE_NUM][LIMIT][F0];
-ll dfs(int i,int pre, bool lim, bool f0, const string& str){
-    if(v[i][pre][f0][lim])  return dp[i][pre][f0][lim];
-    v[i][pre][f0][lim] = true;
-    
-    if(i == str.size())
-        return dp[i][pre][f0][lim] = 1;
-    
-    ll ret = 0, h = lim ? str[i] : '9';
-    
-    for(int j='0'; j<=h; j++){
-        if(abs(j-pre)>=2 || f0){
-            ret += dfs(i+1, j, j==h && lim, f0 && j=='0', str);
-    }}
-    return dp[i][pre][f0][lim] = ret;
+int dp[MXN_LEN][PRE_NUM][LIMIT][F0];
+int DP(const string &s, int i = 0, int pre = 0, bool lim = 1, bool f0 = 1) {
+    if (dp[i][pre][lim][f0]) return dp[i][pre][lim][f0];
+    if (i == s.size()) return dp[i][pre][lim][f0] = 1;
+    int res = 0, h = lim ? s[i] - '0' : 9;
+    for (int j = 0; j <= h; j++)
+        if (abs(j - pre) >= 2 || f0)
+            res += DP(s, i + 1, j, j == h && lim, f0 && !j);
+    return dp[i][pre][lim][f0] = res;
 }
--- dp/p_median.cpp
+++ dp/p_median.cpp
@@ -1,7 +1,7 @@
 void p_Median(){
     for (int i=1; i<=N; ++i)
         for (int j=i; j<=N; ++j){
-            m = (i+j)/2,d[i][j] = 0;        // m是中位數，d[i][j]為距離的總和
+            m = (i+j)/2,d[i][j] = 0; // m是中位數，d[i][j]為距離的總和
             for (int k=i; k<=j; ++k) d[i][j] += abs(arr[k] - arr[m]);
         }
     for (int p=1; p<=P; ++p)
@@ -10,7 +10,7 @@ void p_Median(){
             for (int k=p; k<=n; ++k)
                 if (dp[p-1][k-1] + d[k][n] < dp[p][n]){
                     dp[p][n] = dp[p-1][k-1] + d[k][n];
-                    r[p][n] = k;    // 從第k個位置往右到第j個位置
+                    r[p][n] = k; // 從第k個位置往右到第j個位置
                 }
         }
 }
